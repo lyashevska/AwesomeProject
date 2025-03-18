9473232db9c35eec9320d75c740b71178ca87b01
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2025-03-18 14:28:13 +0100

    Address Olga comment on usage

--- sum.py
+++ sum.py
@@ -1,2 +1,9 @@
 def add(a, b) -> c:
+    """
+    Add two python objects (if summable)
+    Use as:
+    sum_int = sum(1, 6)
+    or:
+    long_string = sum("hello", " world")
+    """
     return a + b
